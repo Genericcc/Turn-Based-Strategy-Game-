f91d93de556c3f65cee4b90d6bb7018716f3f0b3
Genericcc <m.bolan.m@gmail.com>
2022-08-02 14:56:01 +0200

    Added grenade visuals, and randomised ragdoll fall.

--- Assets/Scripts/Unit/UnitRagdoll.cs
+++ Assets/Scripts/Unit/UnitRagdoll.cs
@@ -9,7 +9,11 @@ public class UnitRagdoll : MonoBehaviour
     public void Setup(Transform originalRootBone)
     {
         MatchAllChildTransorms(originalRootBone, ragdollRootBone);
+
+        Vector3 randomDir = new Vector3(Random.Range(-1f, 1f), 0, Random.Range(-1f, 1f));
+        ApplyExplosionToRagdoll(ragdollRootBone, 300f, transform.position + randomDir, 10f);
     }
+
 
     private void MatchAllChildTransorms(Transform root, Transform clone)
     {
@@ -26,4 +30,16 @@ public class UnitRagdoll : MonoBehaviour
         }
     }
     
+    private void ApplyExplosionToRagdoll(Transform root, float explosionForce, Vector3 explosionPosition, float exploasionRange)
+    {
+        foreach(Transform child in root)
+        {
+            if(child.TryGetComponent<Rigidbody>(out Rigidbody childRigidbody))
+            {
+                childRigidbody.AddExplosionForce(explosionForce, explosionPosition, exploasionRange);
+            }
+
+            ApplyExplosionToRagdoll(child, explosionForce, explosionPosition, exploasionRange);
+        }
+    }
 }
